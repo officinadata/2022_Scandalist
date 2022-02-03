7422457c421133a96053de9fc06212df5bb82c3a
jrhut <jamierayhutchings@gmail.com>
2022-02-03 15:50:04 +1300

    MetaGraph User -> Tweet Bipartite Network

--- Julia/Scripts/00_ingestion.jl
+++ Julia/Scripts/00_ingestion.jl
@@ -19,6 +19,7 @@ using Dates
 using Graphs
 using SparseArrays
 using DataStructures
+using MetaGraphs
 
 Pepsi_data = "Data/pepsi.csv" |> read_tw_data
 
@@ -98,25 +99,50 @@ print(first(sort(c_df, :count, rev=true), 20))
 # ROUGH REPLY / QUOTES NETWORK TESTING
 
 pepsi_short = dropmissing(select(Pepsi_data, :created_at, :tweet_id, :author_id, :sourcetweet_id))
-total_nodes = unique(vcat(pepsi_short[!, :author_id], pepsi_short[!, :tweet_id], pepsi_short[!, :sourcetweet_id]))
+total_nodes = unique(string.(vcat(pepsi_short[!, :author_id], pepsi_short[!, :tweet_id], pepsi_short[!, :sourcetweet_id])))
 filter!(x -> x != "NA", total_nodes)
 m = Matrix(pepsi_short)
 
 am = spzeros(Bool, length(total_nodes), length(total_nodes))
 
-authors = unique(pepsi_short[!, :author_id])
-tweets = unique(vcat(pepsi_short[!, :tweet_id], pepsi_short[!, :sourcetweet_id]))
-
 for i = 1:size(pepsi_short, 1)
     if m[i,4] != "NA"
-        index1 = findfirst(==(m[i,3]), authors)
-        am[index1, length(authors) + findfirst(==(m[i,4]), tweets)] = true
+        index1 = findfirst(==(string(m[i,3])), total_nodes)
+        am[index1, findfirst(==(string(m[i,4])), total_nodes)] = true
     end
 end
 
 g = SimpleDiGraph(am)
+mg = MetaGraph(g)
+
+for i = 1:length(total_nodes)
+    set_prop!(mg, i, :id, total_nodes[i])
+end
+
+set_indexing_prop!(mg, :id)
+
+for i = 1:length(unique(pepsi_short[!, :author_id]))
+    set_prop!(mg, i, :type, "accountId")
+end
+
+for i = length(unique(pepsi_short[!, :author_id]))+1:length(total_nodes)
+    set_prop!(mg, i, :type, "tweetId")
+end
+
+for i = 1:size(pepsi_short, 1)
+    if pepsi_short[i,4] != "NA"
+        str = mg[string(pepsi_short[i,3]), :id]
+        dst = mg[string(pepsi_short[i,4]), :id]
+        if haskey(props(mg, str, dst), :datetimes)
+            #println(str, " ", dst)
+            set_prop!(mg, str, dst, :datetimes, push!(get_prop(mg, str, dst, :datetimes), ZonedDateTime(pepsi_short[i,1])))
+        else
+            set_prop!(mg, str, dst, :datetimes, [ZonedDateTime(pepsi_short[i,1])])
+        end
+        #println(get_prop(mg, str, dst, :datetimes))
+        #println(str, " ", dst)
+    end
+end
+
 savegraph("reply_quote_graph.lgz", g)
 g = loadgraph("reply_quote_graph.lgz")
-
-cen_df = DataFrame(user_name=total_nodes, centrality=eigenvector_centrality(g))
-print(first(sort(cen_df, :centrality, rev=true), 20))
