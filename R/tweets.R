9e8ca1f94b7afce0d1be9d802096dd4f39656b68
jrhut <jamierayhutchings@gmail.com>
2022-02-24 11:39:39 +1300

    R for wrangling

---
+++ R/tweets.R
@@ -0,0 +1,20 @@
+install.packages("academictwitteR")
+
+library(academictwitteR)
+library(tidyverse)
+
+dirs <- list.dirs(path = "Data", full.names = TRUE, recursive = TRUE)[-1]
+
+for (dir in dirs) {
+  name <- str_split(dir, "/")[[1]][[2]]
+  
+  tweets_raw <- bind_tweets(data_path = dir)
+  tweets_tidy <- bind_tweets(data_path = dir, output_format = "tidy")
+  
+  ref_types <- tweets_raw %>% mutate(temp = lapply(referenced_tweets, function(x) x[["type"]])) %>% select(temp)
+  ref_ids <- tweets_raw %>% mutate(temp = lapply(referenced_tweets, function(x) x[["id"]])) %>% select(temp)
+  
+  tweets_join <- tweets_tidy %>% mutate(true_type = as.character(flatten(ref_types)), true_id = as.character(flatten(ref_ids)), reply_count = tweets_raw$public_metrics$reply_count)
+  
+  write_csv(tweets_join, paste(name, ".csv", sep="") )
+}
