dc05f98dd8780111ece5718c5166bec140bfb1d4
jrhut <jamierayhutchings@gmail.com>
2022-04-22 18:23:51 +1200

    Refactor code for stats

---
+++ Julia/Scripts/02_plotting_refac.jl
@@ -0,0 +1,119 @@
+using Pkg
+
+cd("Julia")
+Pkg.activate(".")
+Pkg.instantiate()
+
+using Revise
+
+include("01_wrangling.jl")
+include("../src/tweet_helpers_refac.jl")
+include("../src/tweet_graphing.jl")
+
+using Chain
+using CairoMakie
+using AlgebraOfGraphics
+using DataFrames
+using DataFramesMeta
+using Dates
+using Graphs
+using SparseArrays
+using DataStructures
+using MetaGraphs
+using GraphPlot
+
+
+set_aog_theme!()
+
+
+function graph2(df, date_func)
+    fig = Figure()
+
+    g1 = plot_graph(get_column_summary(df, :quote_count, date_func, sum), ["Day of year", "N quotes", "Daily quote volume"], false, fig[1,1])
+    g2 = plot_graph(get_column_summary(df, :retweet_count, date_func, sum), ["Day of year", "N retweets", "Daily retweet volume"], false, fig[1,2])
+    g3 = plot_graph(get_column_summary(df, :reply_count, date_func, sum), ["Day of year", "N replies", "Daily reply volume"], false, fig[2,1])
+    
+    return fig
+end
+
+
+function graph4(df, date_func)
+    fig = Figure()
+
+    g1 = plot_graph(get_tweet_rate(df, :quote_count, date_func, sum), ["Day of year", "Avg n quotes per min", "Daily quote rate"], true, fig[1,1])
+    g2 = plot_graph(get_tweet_rate(df, :retweet_count, date_func, sum), ["Day of year", "Avg n retweets per min", "Daily retweet rate"], true, fig[1,2])
+    g3 = plot_graph(get_tweet_rate(df, :reply_count, date_func, sum), ["Day of year", "Avg n replies per min", "Daily reply rate"], true, fig[2,1])
+ 
+    return fig
+end
+
+
+function graph5(df, date_func)
+    fig = Figure()
+
+    g1 = plot_graph(@transform(get_column_summary(df, :quote_count, date_func, sum), :y = cumsum(:y)), ["Day of year", "Cumulative n quotes", "Daily quote volume"], false, fig[1,1])
+    g2 = plot_graph(@transform(get_column_summary(df, :retweet_count, date_func, sum), :y = cumsum(:y)), ["Day of year", "Cumulative n retweets", "Daily retweet volume"], false, fig[1,2])
+    g3 = plot_graph(@transform(get_column_summary(df, :reply_count, date_func, sum), :y = cumsum(:y)), ["Day of year", "Cumulative n replies", "Daily reply volume"], false, fig[2,1])
+    
+    return fig
+end
+
+
+function _plot_stats(df, date_func, dir)
+    save(dir * "/graph1.png", plot_graph(get_column_summary(df, :author_id, date_func, length ∘ unique), ["Day of year", "N unique users", "Daily unique users"], false), px_per_unit = 3)
+    save(dir * "/graph2.png", graph2(df, date_func), px_per_unit = 3)
+    save(dir * "/graph3.png", plot_graph(get_tweet_rate(df, :author_id, date_func, length ∘ unique), ["Day of year", "N unique users per min", "Daily unique user rate"], true), px_per_unit = 3)
+    save(dir * "/graph4.png", graph4(df, date_func), px_per_unit = 3)
+    save(dir * "/graph5.png", graph5(df, date_func), px_per_unit = 3)
+end
+
+
+function gen_stat_plots(date_func, out_dir)
+    dfs = read_files("Data")
+    scandals = read_names("Data")
+
+    for s in scandals
+        if !isdir(out_dir * "/" * s)
+            mkdir(out_dir * "/" * s)
+        end
+    end
+
+    for i = 1:length(dfs)
+        _plot_stats(dfs[i], date_func, out_dir * "/" * scandals[i])
+    end
+end
+
+
+function _plot_net_met(df, met_funcs, per, cum, dir)
+    g = generate_reply_quote_graph(df)
+    mg = add_meta_reply_quote_graph(df, g)
+    for met_func in met_funcs
+        save(dir * "/" * string(Symbol(met_func)) * ".png", plot_graph_metric(mg, met_func, per, Date.(ZonedDateTime.(df.created_at))[1], Date.(ZonedDateTime.(df.created_at))[size(df,1)], cum), px_per_unit = 3)
+    end
+end
+
+
+function gen_net_plots(met_funcs, per, cum, out_dir)
+    dfs = read_files("Data")
+    scandals = read_names("Data")
+
+    for s in scandals
+        if !isdir(out_dir * "/" * s)
+            mkdir(out_dir * "/" * s)
+        end
+    end
+
+    for i = 1:length(dfs)
+        _plot_net_met(dfs[i], met_funcs, per, cum, out_dir * "/" * scandals[i])
+    end
+end
+
+
+function graph_metric(x)
+    return mean(degree(x))
+end
+
+functions = [graph_metric]
+gen_net_plots(functions, Day(1), false, "Plots2")
+
+gen_stat_plots(dayofyear, "Plots2")
---
+++ Julia/src/tweet_helpers_refac.jl
@@ -0,0 +1,85 @@
+using Chain
+using DataFrames
+using DataFramesMeta
+using Dates
+using StatsBase
+
+
+function get_mentions_by(twit_data::DataFrame, date_func::Function, manip_func::Function = mean)
+    twit_data = add_mentions(twit_data)
+    
+    mentions_by = @chain twit_data begin
+        @transform(:temp = date_func.(:date))
+        @by(:temp, :nmentions = manip_func(:n_mentions))
+    end
+
+    rename!(mentions_by, :temp => Symbol(date_func))
+
+    return mentions_by
+end
+
+
+function add_mentions(twit_data::DataFrame)
+    twit_data.mentions = get_mentions.(lowercase.(twit_data.text))
+    twit_data.n_mentions = length.(twit_data.mentions)
+    
+    return twit_data
+end
+
+
+function make_aog_graph(df::DataFrame)
+    aog = data(df) *
+        visual(Lines) *
+        mapping(Symbol(names(df)[1]), Symbol(names(df)[2]))
+
+    return aog
+end
+
+
+function wrap_in_makie(out_of_AoG, annotations, use_mad = false, fig = Figure())
+    ax1 = Axis(fig[1,1],
+        xlabel = annotations[1],
+        ylabel = annotations[2],
+        title = annotations[3]
+        )
+
+    draw!(ax1, out_of_AoG)
+
+    if use_mad
+        mean_data = mean(out_of_AoG.data[2])
+        mad_data = mad(out_of_AoG.data[2])
+        
+        hlines!(ax1, mean_data)
+        hspan!(ax1, mean_data-mad_data , mean_data+mad_data, color = (:blue, 0.2))
+    end
+    
+    return fig
+end
+
+
+function get_column_summary(twit_data::DataFrame, column::Symbol, date_func::Function, manip_func::Function = sum)
+    df = select(twit_data, :date, column)
+    rename!(df, column => "data")
+    df = @chain df begin
+        @transform(:x = date_func.(:date))
+        @by(:x, :y = manip_func(:data))
+    end
+
+    return df
+end
+
+
+function get_tweet_rate(twit_data::DataFrame, column::Symbol, date_func::Function, manip_function::Function = sum)
+    df = get_column_summary(twit_data, column, date_func, manip_function)
+    df = @chain df begin
+        @transform(:y = :y/1440)
+    end
+    return df
+end
+
+
+function plot_graph(data::DataFrame, annotations, add_mad::Bool = false, f = Figure())
+    aog = make_aog_graph(data)
+    fig = wrap_in_makie(aog, annotations, add_mad, f)
+    return fig
+end
